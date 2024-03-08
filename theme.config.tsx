343082946432743de9bca7b8cd15b841ebb65c86
mazzwell <137079218+mazzwell@users.noreply.github.com>
2024-03-08 17:25:01 +0600

    up

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
+import { useConfig } from 'nextra-theme-docs';
+import Head from 'next/head';
 
 const config: DocsThemeConfig & { useNextSeoProps?: () => object } = {
   logo: <span>Morkie</span>,
@@ -14,8 +17,20 @@ const config: DocsThemeConfig & { useNextSeoProps?: () => object } = {
     text: 'Morkie Docs',
   },
   useNextSeoProps: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+
+    const url =
+      'https://morkie.xyz' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
     return {
       titleTemplate: '%s | Morkie Documentation',
+      meta: [
+        { property: 'og:url', content: url },
+        { property: 'og:title', content: frontMatter.title || 'Morkie Documentation' },
+        { property: 'og:description', content: frontMatter.description || 'A brief overview of our project and what we aim to achieve' }
+      ]
     };
   },
 };
